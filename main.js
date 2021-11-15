ca5c984b61fdf13620c47ede2ee7a2df0a5c58a2
MrJaroo <mishadolininma@mail.ru>
2021-11-15 21:02:52 +0400

    write in js

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-function myAlert (){
-    let  a = 'Error';
-    let b = `<p class="orange">${a}</p>`
-    document.querySelector('.test').innerHTML = b;
+function myAlert(a, c, d) {
+    let b = `<p class="${c}">${a}</p>`
+    document.querySelector(d).innerHTML = b;
 }
 
-myAlert();
+myAlert('misha', 'red', '.test');
+myAlert('Nicolay', 'orange', '.test2')
+
+class Alert {
+    constructor(a, c, d) {
+        this.message = a;
+        this.cssClass = c;
+        this.out = d;
+    }
+    showAlert() {
+        document.querySelector(this.out).innerHTML = `<p class="${this.cssClass}">${this.message}</p>`
+    }
+}
+
+class Alert2 extends Alert {
+    constructor(a, c, d, icon) {
+        super(a, c, d);
+        this.icon = icon
+    }
+    showIconAlert(){
+        
+    }
+}
+
+let m = new Alert('Hello world', 'red', '.test');
+console.log(m);
+m.showAlert();
+
+let m2 = new Alert2('My message','red','.test');
