5a76381f523fdacbad166102623ca209880ddfcc
MrJaroo <mishadolininma@mail.ru>
2021-11-22 18:55:08 +0400

    add /

--- main.js
+++ main.js
@@ -13,20 +13,23 @@
 // console.log(me(45, 5, 15));
 // console.log(12 % 2 == 0)
 
-function bmi(w, h) {
-    let a = w / (h * h)
-    if (a <= 18.5) {
-        return "Underweight"
-    }
-    else if (a <= 25.0) {
-        return "Normal"
-    }
-    else if (a <= 30.0) {
-        return "Overweight"
-    }
-    else if (a > 30) {
-        return "Obese"
-    }
-}
-console.log(bmi(80,1.50))
-bmi(80,1.80);
+// Two exercise
+
+// function bmi(w, h) {
+//     let a = w / (h * h)
+//     if (a <= 18.5) {
+//         return "Underweight"
+//     }
+//     else if (a <= 25.0) {
+//         return "Normal"
+//     }
+//     else if (a <= 30.0) {
+//         return "Overweight"
+//     }
+//     else if (a > 30) {
+//         return "Obese"
+//     }
+// }
+// console.log(bmi(80,1.50))
+// bmi(80,1.80);
+
