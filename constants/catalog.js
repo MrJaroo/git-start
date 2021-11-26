6176fdf8829ec3236102bdc5dfe101a068e77873
MrJaroo <mishadolininma@mail.ru>
2021-11-26 17:43:57 +0400

    add style

---
+++ constants/catalog.js
@@ -0,0 +1,50 @@
+const CATALOG = [
+    {
+        id : 'el1',
+        name: 'J&D ART2',
+        img: 'https://i.ibb.co/jGczVjb/JDART2.jpg',
+        price: '11232',
+    },
+    {
+        id : 'el2',
+        name: 'ESP LTD ALEXI-200 BLK',
+        img: 'https://i.ibb.co/xYgXSxV/ltd-alexi-200b.jpg',
+        price: '59200',
+    },
+    {
+        id : 'el3',
+        name: 'PRS SE Custom 24 FR Charcoal Burst',
+        img: 'https://i.ibb.co/7zgNpmw/1b2743c7c261a14250eb241839cf45f4.jpg',
+        price: '84360',
+    },
+    {
+        id : 'el4',
+        name: 'PRS SE Custom 24 FR Charcoal Burst',
+        img: 'https://i.ibb.co/7zgNpmw/1b2743c7c261a14250eb241839cf45f4.jpg',
+        price: '84360',
+    },
+    {
+        id : 'el5',
+        name: 'ESP LTD BB-600 Baritone SEE THRU Black Sunburst Satin',
+        img: 'https://i.ibb.co/BV6XHng/5a7d65731ecb22671e605b83d3c75666.jpg',
+        price: '113842',
+    },
+    {
+        id : 'el6',
+        name: 'Caparison Horus M3 EF/MF Trans. Spectrum Black',
+        img: 'https://i.ibb.co/LpzYxXL/d0655a168f832b4b19fd35bb3b4613ef.jpg',
+        price: '197828',
+    },
+    {
+        id : 'el7',
+        name: 'ESP LTD SPARROWHAWK BLK El.Guitar w/case',
+        img: 'https://i.ibb.co/BgT7KpG/13874356-800.jpg',
+        price: '109440',
+    },
+    {
+        id : 'el8',
+        name: 'ESP LTD MIRAGE DELUXE 87 Turquoise',
+        img: 'https://i.ibb.co/KwqWwhd/b78e3eae0bc7bfb98977556214f5c53d.jpg',
+        price: '88160',
+    },
+];
